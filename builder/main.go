7bddf0bbeedb68437bbb34df654fac9dab55e5e2
Tom Wiesing <tkw01536@gmail.com>
2025-07-05 13:34:28 +0200

    add more modules

--- builder/main.go
+++ builder/main.go
@@ -10,6 +10,8 @@ import (
 )
 
 var modules = []modsite.Module{
+	modsite.GitForgeModule("go.tkw01536.de/ggman", "https://github.com/tkw1536/ggman", "main"),
+	modsite.GitForgeModule("go.tkw01536.de/goprogram", "https://github.com/tkw1536/goprogram", "main"),
 	modsite.GitForgeModule("go.tkw01536.de/modsite", "https://github.com/tkw1536/go.tkw01536.de", "main"),
 	modsite.GitForgeModule("go.tkw01536.de/pkglib", "https://github.com/tkw1536/pkglib", "main"),
 }
